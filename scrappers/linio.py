71d1f49dec81ca928aeab53c82dee9314c4b8795
victor <victor@ilujdebian.sysdev.co>
2016-10-03 19:34:12 -0500

    Fixed broken search in webpage

--- scrappers/linio.py
+++ scrappers/linio.py
@@ -7,19 +7,20 @@ root_url = 'https://www.linio.com.co/'
 
 def scrape_categories():
     driver = webdriver.PhantomJS()
-    driver.get('https://www.linio.com.co/')
+    driver.get(root_url)
     url = driver.page_source
     soup = BeautifulSoup(url, 'html.parser')
 
     for category in soup.find_all('div', class_='subcategory-menu'):
         category_name = category.div.a.text
+        category_url = category.div.a['href']
         for sub in category.find_all('a', class_='subcategory-title')[2:]:
             sub_title = sub.text
             sub_url = sub['href']
-            scrape_products(sub_url, sub_title)
+            scrape_products(category_name, category_url,sub_url, sub_title)
     driver.quit()
 
-def scrape_products(url, sub_title):
+def scrape_products(cat_name, cat_url, url, sub_title):
      html = requests.get(url).text
      soup = BeautifulSoup(html, 'html.parser')
      page = 1
@@ -31,20 +32,19 @@ def scrape_products(url, sub_title):
              image_url = 'https:'+product.div.a.img['data-lazy']
          else:
              image_url = product.div.a.img['data-lazy']
-         scrape_info_product(product_url, product_name, image_url, url)
+         scrape_info_product(cat_name, cat_url, product_url, product_name, image_url, url, sub_title)
          page = page + 1
          html = requests.get(url + '?page=' + str(page)).text
          soup = BeautifulSoup(html, 'html.parser')
 
-def scrape_info_product(product_url, product_name, image_url, sub_url):
+def scrape_info_product(cat_name, cat_url, product_url, product_name, image_url, sub_url, sub_title):
     html = requests.get(product_url).text
     soup = BeautifulSoup(html, 'html.parser')
-    category = soup.find_all('ol', class_='breadcrumb')[0].find_all('li')[2].a.span.text
     product_images = []
     for image in soup.find_all('div', id='image-product'):
         image_product = 'https:' + image.img['data-lazy']
         product_images.append(image_product)
-    final_object = {'category-title':category, 'subcategory-link':sub_url, 'product_name':product_name, 'product_images':product_images}
+    final_object = {'cat_title':cat_name, 'cat_url':cat_url, 'subcat_url':sub_url, 'subcat_name':sub_title, 'product_name':product_name, 'product_images':product_images}
     print(final_object)
     save_rethink(final_object)
 
