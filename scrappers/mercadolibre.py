e773ef575885376b9ad43dda559c63402a68a4d6
victor <victor@ilujdebian.sysdev.co>
2016-11-21 22:09:34 -0500

    get_product function started

--- scrappers/mercadolibre.py
+++ scrappers/mercadolibre.py
@@ -2,9 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def get_product(url):
+    soup = get_source_code(url)
+    products_list = soup.find_all('h2', class_='list-view-item-title')
+    for product in products_list:
+        if 'list-view-item-title-ad' in product.a['class']:
+            continue
+        else:
+            product_url = product.a['href']
+            print(scrape_product(product_url))
+
 def scrape_product(url):
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = get_source_code(url)
     #The product_title class has two spaces at the end, if something doesn't work, check this.
     title = soup.find('h1', class_='vip-title-main ').text.strip()
     location = soup.find('div', class_='vip-section-header').dd.span.text
@@ -15,3 +24,8 @@ def scrape_product(url):
     selled = int(re.sub('[^\d]', "", selled_unformatted))
     product_object = {'url':url, 'title':title, 'location':location, 'price':price, 'condition':condition, 'selled':selled}
     return product_object
+
+def get_source_code(url):
+    html = requests.get(url).text
+    soup_source = BeautifulSoup(html, 'html.parser')
+    return soup_source
