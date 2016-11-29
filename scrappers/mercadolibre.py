70521728fe84e34c7e59af37c04d115a88ae9004
victor <victor@ilujdebian.sysdev.co>
2016-11-28 19:33:17 -0500

    Added functions to check if html text and number elements exists

--- scrappers/mercadolibre.py
+++ scrappers/mercadolibre.py
@@ -25,28 +25,14 @@ def get_product(url):
                 count = count + 1
                 product_url = product.h2.a['href']
                 product_object = scrape_product(product_url)
-                sold_unknown = product.find('li', class_='extra-info-sold')
-                if sold_unknown is None:
-                    sold = 0
-                else:
-                    sold_unformatted = sold_unknown.text
-                    sold_formatted = int(re.sub('[^\d]', '', sold_unformatted))
-                    sold = sold_formatted
-                condition_unknown = product.find('li', class_='extra-info-condition')
-                if condition_unknown is None:
-                    condition = ""
-                else:
-                    condition = condition_unknown.text
-                location_unknown = product.find('li', class_='extra-info-location')
-                if location_unknown is None:
-                    location = ""
-                else:
-                    location = location_unknown.text
+                sold = check_numb_element(product.find('li', class_='extra-info-sold'))
+                condition = check_text_element(product.find('li', class_='extra-info-condition'))
+                location = check_text_element(product.find('li', class_='extra-info-location'))
                 product_object['sold'] = sold
                 product_object['condition'] = condition
                 product_object['location'] = location
                 print(product_object)
-                save_rethink(product_object)
+                #save_rethink(product_object)
                 print('Product N' + str(count))
         pagination = pagination + 50
         url = url_original + pagination_text + str(pagination)
@@ -57,20 +43,44 @@ def get_product(url):
         else:
             break
 
+
 def scrape_product(url):
+    """Function that receives a product url as parameter and extracts
+    the product title and price
+    """
     soup = get_source_code(url)
-    #The product_title class has two spaces at the end, if something doesn't work, check this.
     title = soup.find('h1', class_='vip-title-main ').text.strip()
-    price_unformatted = soup.find('article', class_='vip-price ch-price').strong.text
-    price = int(re.sub('[^\d]', "", price_unformatted))
+    #The product_title class has two spaces at the end, if something doesn't work, check this.
+    price = check_numb_element(soup.find('article', class_='vip-price ch-price'), 1)
     product_object = {'url':url, 'title':title, 'price':price}
     return product_object
+
 
 def get_source_code(url):
     html = requests.get(url).text
     soup_source = BeautifulSoup(html, 'html.parser')
     return soup_source
 
+
 def save_rethink(final_object):
     r.connect('localhost', 28015).repl()
     r.db('test').table('products').insert(final_object).run()
+
+
+def check_text_element(html_element):
+    if html_element is None:
+        return ""
+    else:
+        return html_element.text
+
+def check_numb_element(html_element, is_price=0):
+    if html_element is None:
+        return 0
+    elif is_price == 0:
+        unformatted_number = html_element.text
+        formatted_number = int(re.sub('[^\d]', '', unformatted_number))
+        return formatted_number
+    elif is_price == 1:
+        html_unformatted = html_element.strong.text
+        html_formatted = int(re.sub('[^\d]', '', html_unformatted))
+        return html_formatted
