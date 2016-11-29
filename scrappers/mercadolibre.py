c9a9f399c138ce712ba4732ef3f48660f033cb8d
victor <victor@ilujdebian.sysdev.co>
2016-11-28 20:18:46 -0500

    Added functions to get categories and subcategories, also check controls for the elements in the webpage

--- scrappers/mercadolibre.py
+++ scrappers/mercadolibre.py
@@ -3,13 +3,28 @@ from bs4 import BeautifulSoup
 import re
 import rethinkdb as r
 
+def get_categories(url_categorie):
+    #Function that receive the web url and get all the categories
+    soup = get_source_code(url_categorie)
+    categories_html = soup.find('div', class_='ch-g4-5')
+    for categorie in categories_html.find_all('li'):
+        url_subcategorie = categorie.a['href']
+        get_subcategories(url_subcategorie)
+
+
 def get_subcategories(url):
+    #Function that receive the categorie url and get all the subcategories
     soup = get_source_code(url)
     subcategories = soup.find('div', class_='nav')
-    for subcat in subcategories:
-        url_sub = 'test'
+    cat_title = subcategories.h1.text
+    for subcategorie in subcategories.find_all('h2'):
+        url_sub = subcategorie.a['href']
+        subcat_title = subcategorie.a.text
+        get_product(url_sub, subcat_title, cat_title)
 
-def get_product(url):
+
+def get_product(url, subcategorie_title, categorie_title):
+    #Function that receive the subcategorie url and get all the products
     url_original = url
     pagination_text = '_Desde_'
     pagination = 1
@@ -31,8 +46,11 @@ def get_product(url):
                 product_object['sold'] = sold
                 product_object['condition'] = condition
                 product_object['location'] = location
+                product_object['subcategorie'] = subcategorie_title
+                product_object['categorie'] = categorie_title
+                product_object['subcategorie_url'] = url
                 print(product_object)
-                #save_rethink(product_object)
+                save_rethink(product_object)
                 print('Product N' + str(count))
         pagination = pagination + 50
         url = url_original + pagination_text + str(pagination)
@@ -57,12 +75,15 @@ def scrape_product(url):
 
 
 def get_source_code(url):
+    """Function that receive the url, then it get the
+    source code and parse it into an python object"""
     html = requests.get(url).text
     soup_source = BeautifulSoup(html, 'html.parser')
     return soup_source
 
 
 def save_rethink(final_object):
+    #Function that connects to rethinkdb and save the object into the table
     r.connect('localhost', 28015).repl()
     r.db('test').table('products').insert(final_object).run()
 
@@ -74,6 +95,9 @@ def check_text_element(html_element):
         return html_element.text
 
 def check_numb_element(html_element, is_price=0):
+    """Function that receives a html element, check if
+    this element is None, or if the element is for price
+    and return the element formatted as an INT"""
     if html_element is None:
         return 0
     elif is_price == 0:
@@ -84,3 +108,5 @@ def check_numb_element(html_element, is_price=0):
         html_unformatted = html_element.strong.text
         html_formatted = int(re.sub('[^\d]', '', html_unformatted))
         return html_formatted
+
+get_categories('http://www.mercadolibre.com.co/')
